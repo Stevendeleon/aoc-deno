48eb58b4024adf2e2787f6e056206f23921a07ad
Steven DeLeon <stevendeleon2892@gmail.com>
2024-12-03 07:13:57 -0500

    day 3 - init complete

--- src/challenges/2024/day03/solution.ts
+++ src/challenges/2024/day03/solution.ts
@@ -1,7 +1,7 @@
 import { readAdjacentFile } from '@/utils/file.ts';
 
 if (import.meta.main) {
-	// const textInput = await readAdjacentFile(import.meta.url, './input.txt');
+	// const textInput = await readAdjacentFile(import.meta.url, './input_sample.txt');
 	//
 	// const result = partOne(textInput);
 	// const result2 = partTwo(textInput);
@@ -11,10 +11,55 @@ if (import.meta.main) {
 }
 
 export function partOne(input: string): number {
-	// ...
-	return 0;
+	const regex = /mul\(([\d.]+),([\d.]+)\)/g;
+	const matches = input.match(regex);
+
+	let sum = 0;
+	const formattedMatches = matches?.map(match => {
+		const matchResult = match.match(/\((\d+),(\d+)\)/);
+		if (matchResult) {
+			const [, x, y] = matchResult;
+			return `${x},${y}`;
+		}
+		return null;
+	}).filter(Boolean)
+
+	for (let i = 0; i < formattedMatches!.length; i++) {
+		const [x, y] = formattedMatches![i]!.split(',');
+		sum += Number(x) * Number(y);
+	}
+
+	return sum;
 }
 export function partTwo(input: string): number {
-	// ...
-	return 0;
+	const regex = /mul\(([\d.]+),([\d.]+)\)|do\(\)|don't\(\)/g;
+
+	let inExclusionZone = false;
+	let match;
+	let sum = 0;
+	const matches: string[] = [];
+
+	while ((match = regex.exec(input)) !== null) {
+		const [fullMatch, arg1, arg2] = match;
+
+		if (fullMatch === "do()") {
+			inExclusionZone = false;
+		}
+
+		if (fullMatch === "don't()") {
+			inExclusionZone = true;
+		}
+
+		if (!inExclusionZone && arg1 !== undefined && arg2 !== undefined) {
+			matches.push(`${arg1},${arg2}`);
+		}
+	}
+	// console.log(matches);
+
+	for (let i = 0; i < matches.length; i++) {
+		const [x, y] = matches[i]!.split(',');
+		sum += Number(x) * Number(y);
+
+	}
+	return sum;
 }
